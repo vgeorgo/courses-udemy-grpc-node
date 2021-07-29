16fc58bd83769b6bb4754bc1da78ed9267296b2d
Victor Georg Oliveira <vgeorgoliveira@gmail.com>
2021-07-28 21:16:50 -0300

    Added GreetingService

--- client/client.js
+++ client/client.js
@@ -1,14 +1,30 @@
 const grpc = require('grpc');
 
-const services = require('../server/protos/dummy_grpc_pb');
+const { GreetRequest, Greeting } = require('../server/protos/greet_pb');
+const { GreetServiceClient } = require('../server/protos/greet_grpc_pb');
 
 const main = () => {
-  const client = new services.DummyServiceClient(
+  const client = new GreetServiceClient(
     'localhost:50051',
     grpc.credentials.createInsecure(),
   );
 
-  console.log('Client started');
+  // Protocol buffer Greeting message
+  const greeting = new Greeting();
+  greeting.setFirstName('Victor');
+  greeting.setLastName('Georg Oliveira');
+
+  const request = new GreetRequest();
+  request.setGreeting(greeting);
+
+  client.greet(request, (err, r) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(r.getResult());
+  });
 };
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const grpc = require('grpc');
 
+const { GreetResponse } = require('./protos/greet_pb');
+const { GreetServiceService } = require('./protos/greet_grpc_pb');
+
+/**
+ * Implements greet RPC method
+ */
+const greet = (call, callback) => {
+  const greeting = new GreetResponse();
+  greeting.setResult(
+    `Hello ${call.request.getGreeting().getFirstName()} ${call.request.getGreeting().getLastName()}`
+  );
+  callback(null, greeting);
+};
+
 const main = () => {
   const server = new grpc.Server();
   const serverAddr = '127.0.0.1:50051';
 
+  // Services
+  server.addService(GreetServiceService, { greet });
+
+  // Server start
   server.bind(serverAddr, grpc.ServerCredentials.createInsecure());
   server.start();
   console.log(`Server running on ${serverAddr}`);
